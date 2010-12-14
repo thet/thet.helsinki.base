4c0b7951aaade4bf4ea748fbf7bb299d35108d5e
Johannes Raggam <raggam-nl@adm.at>
2010-12-14 15:33:56 +0100

    helsinki base initial

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='thet.helsinki.base',
+      version=version,
+      description="base plone integration package for radio helsinki",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone4, zope',
+      author='Johannes Raggam',
+      author_email='<raggam-nl@adm.at>',
+      url='https://github.com/thet/thet.helsinki.base',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['thet.helsinki'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+	  'Plone',
+      ],
+      )
