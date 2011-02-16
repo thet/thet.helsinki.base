19a8798be70d9f5bdf403e97f484d2c89196b46a
Johannes Raggam <raggam-nl@adm.at>
2011-02-16 01:27:25 +0100

    remove unecessary

--- thet/helsinki/base/setuphandlers.py
+++ thet/helsinki/base/setuphandlers.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-#
-# GNU General Public License (GPL)
-#
-__author__ = """Johannes Raggam <johannes@raggam.co.at>"""
-__docformat__ = 'plaintext'
-
 import collective.setuphandlertools as sht
 import logging
 logger = logging.getLogger("thet.helsinki.base")
