4338e6a8ad926c18f4e8729f84cdd6b6d05fb636
Johannes Raggam <raggam-nl@adm.at>
2012-11-07 11:35:42 +0100

    remove IPortletAvailable adapter

--- thet/helsinki/base/portlet_adapters.py
+++ thet/helsinki/base/portlet_adapters.py
@@ -1,29 +1,10 @@
-from Acquisition import aq_parent
-from Products.CMFPlone.interfaces.siteroot import IPloneSiteRoot
-from collective.teaser.interfaces import IPortletAvailable
-from zope.interface import implementer, implements, Interface
+from zope.interface import implements, Interface
 from zope.component import adapts
 from plone.app.portlets.portlets.navigation import QueryBuilder
 from plone.app.layout.navigation.interfaces import INavigationQueryBuilder
 from plone.app.portlets.portlets.navigation import INavigationPortlet
 from plone.app.layout.navigation.root import getNavigationRoot
 
-@implementer(IPortletAvailable)
-def main_teaser_available(portlet, manager, context):
-    if IPloneSiteRoot.providedBy(context) or\
-       IPloneSiteRoot.providedBy(aq_parent(context)) and\
-       aq_parent(context).getDefaultPage() == context.id:
-        # wether plone root or a default page of plone root
-        return True
-    return False
-
-@implementer(IPortletAvailable)
-def portlet_enabled(portlet, manager, context):
-    return True
-
-@implementer(IPortletAvailable)
-def portlet_disabled(portlet, manager, context):
-    return False
 
 class CustomQueryBuilder(QueryBuilder):
     implements(INavigationQueryBuilder)
