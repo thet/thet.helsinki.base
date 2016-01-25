4c023eb48757e51822b4500b4f35053ca948f93a
Johannes Raggam <thetetet@gmail.com>
2016-01-25 11:42:31 +0100

    cleanup

---
+++ README.rst
@@ -0,0 +1,6 @@
+Introduction
+============
+
+Plone integration package for Radio Helsinki - freies Radio Graz.
+
+
--- setup.py
+++ setup.py
@@ -3,46 +3,42 @@ import os
 
 version = '1.0'
 
-setup(name='thet.helsinki.base',
-      version=version,
-      description="base plone integration package for radio helsinki",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='thet.helsinki.base',
+    version=version,
+    description="Plone integration package for Radio Helsinki Graz',
+    long_description=open("README.rst").read(),
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        ],
-      keywords='plone4, zope',
-      author='Johannes Raggam',
-      author_email='<raggam-nl@adm.at>',
-      url='https://github.com/thet/thet.helsinki.base',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['thet', 'thet.helsinki'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-          'Plone',
-          'Pillow',
-          'z3c.jbot',
-          'plone.app.caching',
-          #'plone.formwidget.recaptcha',
-          #'collective.autoresizetextarea',
-          'Products.LinguaPlone',
-          'collective.folderishtypes',
-          'collective.folderishtraverse',
-          'collective.folderorder',
-          'collective.teaser',
-          'thet.helsinki.project',
-          'collective.gcs',
-          #'webcouturier.dropdownmenu',
-          #'collective.carousel',
-          'plone.app.contenttypes < 1.2',
-          'collective.plonetruegallery',
-          'collective.quickupload'
-      ],
+    ],
+    keywords='plone4, zope',
+    author='Johannes Raggam',
+    author_email='<thetetet@gmail.com>',
+    url='https://github.com/thet/thet.helsinki.base',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['thet', 'thet.helsinki'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+        'Plone',
+        'Pillow',
+        'z3c.jbot',
+        'plone.app.caching',
+        #'plone.formwidget.recaptcha',
+        #'collective.autoresizetextarea',
+        'Products.LinguaPlone',
+        'collective.folderishtypes',
+        'collective.folderishtraverse',
+        'collective.folderorder',
+        'collective.teaser',
+        'thet.helsinki.project',
+        'collective.gcs',
+        'plone.app.contenttypes < 1.2',
+        'collective.plonetruegallery',
+        'collective.quickupload'
+    ],
 )
